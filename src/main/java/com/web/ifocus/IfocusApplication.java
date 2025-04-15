3f13bbe8e816adb6335927c5c7f9a1d33bb0dc9f
Alexander Saul <alexandersaul2604@gmail.com>
2025-04-14 22:51:04 -0500

    First commit

---
+++ src/main/java/com/web/ifocus/IfocusApplication.java
@@ -0,0 +1,13 @@
+package com.web.ifocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfocusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/ifocus/IfocusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.ifocus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfocusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
